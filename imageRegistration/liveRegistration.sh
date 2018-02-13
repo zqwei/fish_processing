6d982547cb51380e91e835f47c3d1cb62c6eac59
Ziqiang Wei <weiz@janelia.hhmi.org>
2018-02-13 14:19:47 -0500

    fix error in live registration code

--- imageRegistration/liveRegistration.sh
+++ imageRegistration/liveRegistration.sh
@@ -6,24 +6,26 @@ cnvSyN=[200x200x200x10,1e-7,10]
 smth=4x3x2x1vox
 down=12x8x4x2
 smthSyN=4x3x2x1x0vox
-down=12x8x4x2x1
-antsRegistration --dimensionality 3--float 0 \
-                 --output [out_${m%%.*}_,out_${m%%.*}_Warped.nii.gz] \
+downSyN=12x8x4x2x1
+verb=0
+
+antsRegistration --dimensionality 3 --float 1 --verbose $verb \
+                 --output [output/${m%%.*}_,out_${m%%.*}_Warped.nii.gz] \
                  --interpolation WelchWindowedSinc \
                  --use-histogram-matching 0 \
                  --initial-moving-transform [$f,$m,1] \
-                 --transform Rigid[0.1]\
-                 --metric MI[ref/vglut-ref.nii,fish1–01.nii.gz,1,32,Regular,0.25] \
-                 --convergence $cnv \
-                 --shrink-factors $down \
-                 --smoothing-sigmas $smth \
-                 --transform Affine[0.1]
+                 --transform Rigid[0.1] \
                  --metric MI[$f,$m,1,32,Regular,0.25] \
                  --convergence $cnv \
                  --shrink-factors $down \
-                 --smoothing-sigmas $smth \
-                 --transform SyN[0.05,6,0.5] \
-                 --metric CC[$f,$m,1,2] \
-                 --convergence $cnvSyN \
-                 --shrink-factors $downSyN \
-                 --smoothing-sigmas $smthSyN
+                 --smoothing-sigmas $smth
+                 # --transform Affine[0.1] \
+                 # --metric MI[$f,$m,1,32,Regular,0.25] \
+                 # --convergence $cnv \
+                 # --shrink-factors $down \
+                 # --smoothing-sigmas $smth \
+                 # --transform SyN[0.05,6,0.5] \
+                 # --metric CC[$f,$m,1,2] \
+                 # --convergence $cnvSyN \
+                 # --shrink-factors $downSyN \
+                 # --smoothing-sigmas $smthSyN
