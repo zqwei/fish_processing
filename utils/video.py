8b14f339bc7608082b7566f0e08d7e443f8e4bb8
Ziqiang Wei <weiz@c11u24.int.janelia.org>
2018-04-05 00:03:36 -0400

    update video code for time at first or end axis

--- utils/video.py
+++ utils/video.py
@@ -1,7 +1,9 @@
 import imageio
 import numpy as np
 
-def videoWrite(arr, fileName, fps=30):
+def videoWrite(arr, fileName, fps=30, axis=0):
+    if axis ==-1:
+        arr = arr.transpose([2, 0, 1])
     arr_ = (arr - arr.min())/(arr.max()-arr.min())*255
     with imageio.get_writer(fileName, fps=fps) as writer:
         for im in arr_:
