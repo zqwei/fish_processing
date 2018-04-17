fe286550ff23a759e31134be0f00ea5f302b1b1c
Ziqiang Wei <weiz@janelia.hhmi.org>
2018-04-17 18:00:53 -0400

    update video generation code

--- utils/video.py
+++ utils/video.py
@@ -1,10 +1,23 @@
 import imageio
+import cv2
 import numpy as np
 
 def videoWrite(arr, fileName, fps=30, axis=0):
     if axis ==-1:
         arr = arr.transpose([2, 0, 1])
     arr_ = (arr - arr.min())/(arr.max()-arr.min())*255
+    arr_ = arr_.astype('uint8')
     with imageio.get_writer(fileName, fps=fps) as writer:
         for im in arr_:
             writer.append_data(im)
+
+def writeVideoFile(images, nfile_name, frameRate = 30):
+    # using opencv to write a movie
+    tlen, ImageWidth, ImageHeight = images.shape
+    writer = cv2.VideoWriter(nfile_name + '.MJPEG', cv2.VideoWriter_fourcc(*'MJPG'), frameRate, (ImageWidth, ImageHeight), False)
+    images = (images - images.min())/(images.max() - images.min())*255
+    images = images.astype('uint8')
+    for img in images:
+        writer.write(img)
+    writer.release()
+
