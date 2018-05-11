9c8abb7395f0bd197355874e7ddb11e58f8378ef
Ziqiang Wei <weiz@janelia.hhmi.org>
2018-05-10 22:00:19 -0400

    add neural network spike detector

---
+++ spikeDetectionNN/spikeDetector.py
@@ -0,0 +1,59 @@
+'''
+Copyright 2018 Wei, Ziqiang, Janelia Research Campus
+
+weiz@janelia.hhmi.org
+
+Acknowledgement for example code
+https://gist.github.com/philipperemy/b8a7b7be344e447e7ee6625fe2fdd765
+'''
+import numpy as np
+from keras.layers import RepeatVector, Bidirectional, TimeDistributed
+from keras.layers.core import Dropout, Dense
+from keras.layers.recurrent import LSTM
+from keras.models import Sequential
+from keras.models import load_model
+from keras.utils import plot_model
+
+
+
+def prepare_sequences(x_train, y_train, window_length):
+    full_seq = x_train.flatten()
+    target_seq = y_train.flatten()
+    windows = []
+    outliers = []
+    for window_start in range(0, len(full_seq) - window_length + 1):
+        window_end = window_start + window_length
+        window_range = range(window_start, window_end)
+        window = list(full_seq[window_range])
+        contain_outlier = target_seq[window_range].sum()>0
+        outliers.append(contain_outlier)
+        windows.append(window)
+    return np.expand_dims(np.array(windows), axis=2), np.array(outliers).astype(np.bool)
+
+
+
+# this does not work
+def prepare_sequences_center(x_train, y_train, window_length):
+    full_seq = x_train.flatten()
+    target_seq = y_train.flatten()
+    windows = []
+    outliers = []
+    for window_start in range(0, len(full_seq) - window_length + 1):
+        window_end = window_start + window_length
+        window_range = range(window_start, window_end)
+        window_mid = int(round(np.median(window_range)))
+        window = list(full_seq[window_range])
+        contain_outlier = target_seq[window_mid]>0
+        outliers.append(contain_outlier)
+        windows.append(window)
+    return np.expand_dims(np.array(windows), axis=2), np.array(outliers).astype(np.bool)
+
+
+def create_lstm_model(hidden_dim, window_length, m=1):
+    model = Sequential()
+    model.add(Bidirectional(LSTM(hidden_dim, return_sequences=True), input_shape=(window_length, m)))
+    model.add(Dropout(p=0.2))
+    model.add(Bidirectional(LSTM(hidden_dim, return_sequences=False)))
+    model.add(Dense(1, activation='sigmoid'))
+    model.compile(loss='binary_crossentropy', optimizer='adam')
+    return model
---
+++ spikeDetectionNN/utils.py
@@ -0,0 +1,94 @@
+'''
+Copyright 2018 Wei, Ziqiang, Janelia Research Campus
+
+weiz@janelia.hhmi.org
+'''
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+sns.set(font_scale=1.5)
+sns.set_style("ticks")
+import numpy as np
+import peakutils
+
+
+def plot_spks(plt, spkcount, ratio, label='Raw spike time'):
+    spkTime = spkcount>0
+    plt.plot(np.array(np.where(spkTime)).T, spkcount[spkTime]*ratio,'o', label=label)
+
+
+def plot_test_performance(m, x_test, labels):
+    pred_x_test = m.predict(x_test)
+    labels = labels.astype(np.bool)
+    sns.distplot(pred_x_test[labels], label='Spike')
+    sns.distplot(pred_x_test[~labels], label='None-Spike')
+    sns.despine()
+    plt.xlim([0,1])
+    plt.xlabel('Predition of spike probability')
+    plt.ylabel('Counts')
+    plt.legend()
+    plt.show()
+
+def detected_max_spike(m, x_, voltr_, thres=0.4):
+    pred_x_test = m.predict(x_)
+    window_length = x_.shape[1]
+    spkInWindow = pred_x_test>thres
+    spkInWindow = spkInWindow.flatten()
+    spkInWindow[:window_length] = False
+    spkcount_ = np.zeros(voltr_.shape[0]).astype(np.bool)
+    for idx, nspk in enumerate(spkInWindow):
+        if nspk:
+            x__ = voltr_[idx:idx+window_length]
+            spk_idx = np.where(x__ == x__.max()).squeeze()[0]
+            spkcount_[idx+spk_idx] = True
+    # diff = voltr_set - devoltr_set
+    # diff_ = diff[spkcount_.astype(np.bool)]
+    # thres = np.percentile(diff_, 20)
+    # spk_diff = diff > thres
+    # spkcount__ = np.logical_and(spkcount_, spk_diff)
+    return spkcount_
+
+def detected_peak_spikes(m, x_, voltr_, thres=0.4, devoltr_ = None):
+    pred_x_test = m.predict(x_)
+    window_length = x_.shape[1]
+    spkInWindow = pred_x_test>thres
+    spkInWindow = spkInWindow.flatten()
+    spkInWindow[:window_length] = False
+    spkcount_ = np.zeros(voltr_.shape[0]).astype(np.bool)
+    for idx, nspk in enumerate(spkInWindow):
+        if nspk:
+            x__ = voltr_[idx:idx+window_length]
+            spk_idx = peakutils.indexes(x__, thres=.9, min_dist=10)
+            spkcount_[idx+spk_idx] = True
+    if devoltr_ is not None:
+        diff = voltr_ - devoltr_
+        diff_ = diff[spkcount_.astype(np.bool)]
+        thres = np.percentile(diff_, 20)
+        spk_diff = diff > thres
+        spkcount_ = np.logical_and(spkcount_, spk_diff)
+    return spkcount_
+
+def print_spike_detection_report(spk_in_range, spkc_in_range, title_string):
+    matched = spk_in_range == spkc_in_range
+    tot_ = len(spk_in_range)
+    totspk = spk_in_range.sum()
+    totspk_ = spkc_in_range.sum()
+
+    matched_ = (matched).sum()
+    idx = np.array(np.where(~matched)).flatten()
+    idx_ = idx[1:] - idx[:-1]
+    match1_ = (idx_==1).sum()*2
+    match5_ = np.logical_and(idx_>1, idx_<6).sum()*2
+
+    TPspk = 0
+    winSize = 3
+    for nspk in np.array(np.where(spk_in_range)).flatten():
+        if spkc_in_range[max(0, nspk-winSize):min(nspk+winSize, len(spkc_in_range))].sum()>0:
+            TPspk +=1
+    print('-------------------')
+    print(title_string)
+    print('Total ephys spikes %d'%(totspk))
+    print('Total detected spikes %d'%(totspk_))
+    print('Found spikes %d'%(TPspk))
+    print('Unfound spikes %d'%(totspk - TPspk))
+    print('Extra spikes %d'%(totspk_ - TPspk))
