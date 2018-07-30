560c70a8820de6f96664492d9a75fd41a03e2d6e
Ziqiang Wei <weiz@janelia.hhmi.org>
2018-07-29 23:23:36 -0400

    update testing code for `np_mp.py`

--- utils/np_mp.py
+++ utils/np_mp.py
@@ -1,8 +1,13 @@
-import multiprocessing as mp
-import numpy as np
 '''
 Multiprocessing the function along the axis=0 by spliting the array into chucks
+------------------------
+Ziqiang Wei @ 2018
+weiz@janelia.hhmi.org
 '''
+
+import multiprocessing as mp
+import numpy as np
+
 
 def parallel_to_chunks(func1d, arr, *args, **kwargs):
     if mp.cpu_count() == 1:
@@ -18,6 +23,7 @@ def parallel_to_chunks(func1d, arr, *args, **kwargs):
     pool.join()
 
     results = ()
+    # print(len(individual_results[0]))
     for i_tuple in range(len(individual_results[0])):
         results = results + (np.concatenate([_[i_tuple] for _ in individual_results]), )
     return results
@@ -31,12 +37,21 @@ def unpacking_apply_func(list_params):
 # this is a testing function
 def print_shape(arr):
     print(arr.shape)
-    return np.array([0]), np.array([1])
+    return np.array([0]), np.array([1]),
 
-def test():
+def print_single_return(arr):
+    print(arr.shape)
+    return arr,
+
+def test_():
     x = np.random.rand(100, 10, 2)
     arr0, arr1 = parallel_to_chunks(print_shape, x)
     print(arr0)
 
+def test():
+    x = np.random.rand(100, 10, 2)
+    arr0 = parallel_to_chunks(print_single_return, x)
+    print(arr0)
+
 if __name__ == '__main__':
-    test()
+    test_()
