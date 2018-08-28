665919763cfd63b5167c67a16e7ec905be238996
Ziqiang Wei <weiz@janelia.hhmi.org>
2018-08-28 14:53:43 -0400

    update pad_index function to avoid stripping in the denoise data

--- cdenoise.py
+++ cdenoise.py
@@ -1,4 +1,4 @@
-
+import numpy as np
 
 
 def low_rank_svd(img, rank_k):
@@ -9,19 +9,30 @@ def low_rank_svd(img, rank_k):
     return U.dot(np.diag(s).dot(Vt)).reshape((dx, dy, dt))
 
 def pad_index(arr, overlap_step, max_value):
-    if arr[0]>overlap_step:
-        arr = np.append(arr, arr[0]+np.arange(-overlap_step,0))
-    if arr.max()<max_value-overlap_step:
-        arr = np.append(arr, arr.max()+np.arange(1,overlap_step))
-    return np.sort(arr.flatten())
+    # if arr[0]>overlap_step:
+    #     arr = np.append(arr, arr[0]+np.arange(-overlap_step,0))
+    # if arr.max()<max_value-overlap_step:
+    #     arr = np.append(arr, arr.max()+np.arange(1,overlap_step))
+    # return np.sort(arr.flatten())
+    if overlap_step>0:
+        arr1 = [_-overlap_step for _ in arr]
+        arr2 = [_+overlap_step for _ in arr]
+        for _ in arr2:
+            arr1.append(_)
+        arr1[0] = arr1[0][arr1[0]>=0]
+        arr1[-1] = arr1[-1][arr1[-1]<max_value]
+        return arr1
+    else:
+        return arr
 
 def split_to_blocks(nsize, nblocks, overlap_step):
     assert len(nsize) == len(overlap_step), print("lengths not matched")
     arr = []
     for nsize_, nblock_, overlap_ in zip(nsize, nblocks, overlap_step):
         arr_ = np.array_split(range(nsize_), nblock_)
-        arr_ = [pad_index(_, overlap_, nsize_) for _ in arr_]
-        arr.append(arr_)
+        # arr_ = [pad_index(_, overlap_, nsize_) for _ in arr_]
+        # arr.append(arr_)
+        arr.append(pad_index(arr_, overlap_, nsize_))
     return arr
 
 def get_blocks_from_index_arr(imgStack, arrs):
@@ -58,8 +69,44 @@ def run_single(blocks, maxlag=5, confidence=0.999, greedy=False, fudge_factor=0.
     Yds = [out_[0] for out_ in c_outs]
     vtids = [out_[1] for out_ in c_outs]
     vtids = np.asarray(vtids).astype('int')
-    print('Blocks(=%d) run time: %f'%(len(Y),time.time()-start))
+    print('Blocks(=%d) run time: %f'%(len(blocks),time.time()-start))
     return Yds, vtids
+
+def gpca_to_file(patch_index, files='', maxlag=5, confidence=0.999, greedy=False, fudge_factor=0.99, mean_th_factor=1.15,
+                 U_update=False, min_rank=1, stim_knots=None, stim_delta=200):
+    from . import greedyPCA as gpca
+    patch = patch_index[0]
+    nfile = patch_index[1]
+    c_out = gpca.denoise_patch(patch, maxlag=maxlag, confidence=confidence, greedy=greedy,
+                               fudge_factor=fudge_factor, mean_th_factor=mean_th_factor,
+                               U_update=U_update, min_rank=min_rank, stim_knots=stim_knots,
+                               stim_delta=stim_delta)
+    np.savez(files + '%06d'%(nfile), Yds=c_out[0], vtids=c_out[1])
+    return None
+
+def run_single_to_files(blocks, files, maxlag=5, confidence=0.999, greedy=False, fudge_factor=0.99, mean_th_factor=1.15,
+               U_update=False, min_rank=1, stim_knots=None, stim_delta=200):
+    import time
+    import multiprocessing
+    from functools import partial
+
+    func = partial(gpca_to_file, files=files, maxlag=maxlag, confidence=confidence, greedy=greedy,
+                   fudge_factor=fudge_factor, mean_th_factor=mean_th_factor, U_update=U_update,
+                   min_rank=min_rank, stim_knots=stim_knots, stim_delta=stim_delta)
+
+    start=time.time()
+    cpu_count = multiprocessing.cpu_count()
+    args=[[patch[0], n_] for n_, patch in enumerate(blocks)]
+    start=time.time()
+    pool = multiprocessing.Pool(cpu_count)
+    print('Running %d blocks in %d cpus'%(len(blocks), cpu_count))#if verbose else 0
+    # define params in function
+    pool.map(func, args)
+    pool.close()
+    pool.join()
+    print('Blocks(=%d) run time: %f'%(len(blocks),time.time()-start))
+    return None
+
 
 def combine_blocks(block_data, block_size, block_corrs):
     block_mat = np.zeros(block_size)
@@ -69,3 +116,18 @@ def combine_blocks(block_data, block_size, block_corrs):
         block_mat[z_arr.min():z_arr.max()+1, x_arr.min():x_arr.max()+1, y_arr.min():y_arr.max()+1, :] += ndata
         block_count[z_arr.min():z_arr.max()+1, x_arr.min():x_arr.max()+1, y_arr.min():y_arr.max()+1, :] += 1
     return block_mat, block_count
+
+
+def combine_blocks_from_files(block_data_files, block_size, block_corrs):
+    block_mat = np.zeros(block_size)
+    block_count = block_mat.copy().astype(np.int)
+    block_ranks = block_mat.copy().astype(np.int)
+    for nfile, ncorr in zip(block_data_files, block_corrs):
+        _ = np.load(nfile)
+        ndata = _['Yds']
+        nrank = _['vtids'].astype('int')
+        _, z_arr, x_arr, y_arr = ncorr
+        block_mat[z_arr.min():z_arr.max()+1, x_arr.min():x_arr.max()+1, y_arr.min():y_arr.max()+1, :] += ndata
+        block_count[z_arr.min():z_arr.max()+1, x_arr.min():x_arr.max()+1, y_arr.min():y_arr.max()+1, :] += 1
+        block_ranks[z_arr.min():z_arr.max()+1, x_arr.min():x_arr.max()+1, y_arr.min():y_arr.max()+1, :] += nrank
+    return block_mat, block_count, block_ranks
---
+++ detrend.py
@@ -0,0 +1,80 @@
+from trefide.preprocess import _get_spline_trend
+import numpy as np
+from scipy.signal import butter, lfilter
+
+def detrend(mov,
+            stim=None,
+            disc_idx=None,
+            order=3,
+            followup=100,
+            spacing=200,
+            q=.05,
+            axis=-1,
+            robust=True):
+    # Adaptive spline fit
+
+    if stim is None:
+        stim = np.zeros(mov.shape[-1])
+
+    trend = _get_spline_trend(data=mov,
+                              stim=stim,
+                              disc_idx=disc_idx,
+                              order=order,
+                              followup=followup,
+                              spacing=spacing,
+                              q=q,
+                              axis=axis,
+                              robust=robust)
+
+    mov_detr = np.subtract(mov, trend)
+    # Remove samples from discontinuity locations
+    if disc_idx is not None:
+        del_idx = np.sort(np.append(np.append(disc_idx, disc_idx + 1),
+                                    disc_idx - 1))
+        stim = np.delete(stim, del_idx)
+        mov_detr = np.delete(mov_detr, del_idx, axis=-1)
+        trend = np.delete(trend, del_idx, axis=-1)
+        # Recompute problem areas
+        disc_idx[1:] = disc_idx[1:] - np.cumsum(np.ones(len(disc_idx) - 1) * 3)
+        disc_idx = disc_idx - 1
+        disc_idx = np.append(disc_idx,
+                             np.argwhere(filt.convolve1d(stim > 0,
+                                                         np.array([1, -1]))))
+    return mov_detr, trend, stim, np.unique(disc_idx)
+
+
+def trend(mov, order=3, followup=100, spacing=200, q=.05, axis=-1, robust=True):
+    # Adaptive spline fit
+    stim = np.zeros(mov.shape[-1])
+    return _get_spline_trend(data=mov, stim=stim, disc_idx=None, order=order,
+                             followup=followup, spacing=spacing, q=q, axis=axis, robust=robust),
+
+
+def trend_sg(mov, win_=201, order=7):
+    from scipy.signal import savgol_filter
+    trend = savgol_filter(Y, win_, order, axis=-1)
+    return trend
+
+def butter_bandpass(lowcut, highcut, fs, order=5):
+    nyq = 0.5 * fs
+    low = lowcut / nyq
+    high = highcut / nyq
+    b, a = butter(order, [low, high], btype='band')
+    return b, a
+
+
+def butter_bandpass_filter(data, lowcut, highcut, fs, order=5):
+    b, a = butter_bandpass(lowcut, highcut, fs, order=order)
+    y = lfilter(b, a, data)
+    return y
+
+def butter_lowpass(cutoff, fs, order=5):
+    nyq = 0.5 * fs
+    normal_cutoff = cutoff / nyq
+    b, a = butter(order, normal_cutoff, btype='low', analog=False)
+    return b, a
+
+def butter_lowpass_filter(data, cutoff, fs, order=5):
+    b, a = butter_lowpass(cutoff, fs, order=order)
+    y = lfilter(b, a, data)
+    return y
