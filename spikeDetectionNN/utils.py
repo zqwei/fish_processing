8759003cc29eb37104a37fc2961e7bd4755fb92b
Ziqiang Wei <weiz@janelia.hhmi.org>
2018-09-27 15:45:30 -0400

    update code for peak detection

--- spikeDetectionNN/utils.py
+++ spikeDetectionNN/utils.py
@@ -4,14 +4,14 @@ Copyright 2018 Wei, Ziqiang, Janelia Research Campus
 weiz@janelia.hhmi.org
 '''
 
+import numpy as np
+import deprecation
+from statsmodels.robust.scale import mad
+from scipy.stats import norm as Gaussian
 import matplotlib.pyplot as plt
 import seaborn as sns
 sns.set(font_scale=1.5)
 sns.set_style("ticks")
-import numpy as np
-import peakutils
-from statsmodels.robust.scale import mad
-from scipy.stats import norm as Gaussian
 
 
 def mad_scale(x, axis=0, c=Gaussian.ppf(3/4.)):
@@ -33,64 +33,21 @@ def plot_test_performance(m, x_test, labels, plt=plt):
     plt.ylabel('Counts')
     plt.legend()
 
-    
-def detected_window_max_spike(m, x_, voltr_, thres=0.4):
-    pred_x_test = m.predict(x_)
-    window_length = x_.shape[1]
-    spkInWindow = pred_x_test>thres
-    spkInWindow = spkInWindow.flatten()
-    spkInWindow[:window_length] = False
-    spkcount_ = np.zeros(voltr_.shape[0]).astype(np.bool)
-    for idx, nspk in enumerate(spkInWindow):
-        if nspk:
-            x__ = voltr_[idx:idx+window_length]
-            spk_idx = np.where(x__ == x__.max()).squeeze()[0]
-            spkcount_[idx+spk_idx] = True
-    # diff = voltr_set - devoltr_set
-    # diff_ = diff[spkcount_.astype(np.bool)]
-    # thres = np.percentile(diff_, 20)
-    # spk_diff = diff > thres
-    # spkcount__ = np.logical_and(spkcount_, spk_diff)
-    return spkcount_    
 
-def detected_max_spike(m, x_, voltr_, thres=0.4):
+def detected_window_max_spike(m, x_, voltr_, peak_wid=2, thres=0.5):
     pred_x_test = m.predict(x_)
-    window_length = x_.shape[1]
     spkInWindow = pred_x_test>thres
     spkInWindow = spkInWindow.flatten()
+    window_length = x_.shape[1]
+    hwin = np.median(np.arange(window_length))
     spkInWindow[:window_length] = False
     spkcount_ = np.zeros(voltr_.shape[0]).astype(np.bool)
     for idx, nspk in enumerate(spkInWindow):
         if nspk:
-            x__ = voltr_[idx:idx+window_length]
-            spk_idx = np.where(x__ == x__.max()).squeeze()[0]
-            spkcount_[idx+spk_idx] = True
-    # diff = voltr_set - devoltr_set
-    # diff_ = diff[spkcount_.astype(np.bool)]
-    # thres = np.percentile(diff_, 20)
-    # spk_diff = diff > thres
-    # spkcount__ = np.logical_and(spkcount_, spk_diff)
+            x__ = voltr_[idx+hwin-peak_wid:idx+hwin+peak_wid+1]
+            spkcount_[idx+np.argmax(x__)] = True # first max is set to be spike time
     return spkcount_
 
-def detected_peak_spikes(m, x_, voltr_, thres=0.4, devoltr_ = None, peakThres=.9, peak_minDist=10, smallPeakThres = 20):
-    pred_x_test = m.predict(x_)
-    window_length = x_.shape[1]
-    spkInWindow = pred_x_test>thres
-    spkInWindow = spkInWindow.flatten()
-    spkInWindow[:window_length] = False
-    spkcount_ = np.zeros(voltr_.shape[0]).astype(np.bool)
-    for idx, nspk in enumerate(spkInWindow):
-        if nspk:
-            x__ = voltr_[idx:idx+window_length]
-            spk_idx = peakutils.indexes(x__, thres=peakThres, min_dist=peak_minDist)
-            spkcount_[idx+spk_idx] = True
-    if (devoltr_ is not None) and spkcount_.sum()>0:
-        diff = voltr_ - devoltr_
-        diff_ = diff[spkcount_.astype(np.bool)]
-        thres = np.percentile(diff_, smallPeakThres)
-        spk_diff = diff > thres
-        spkcount_ = np.logical_and(spkcount_, spk_diff)
-    return spkcount_
 
 def print_spike_detection_report(spk_in_range, spkc_in_range, title_string):
     matched = spk_in_range == spkc_in_range
@@ -116,3 +73,46 @@ def print_spike_detection_report(spk_in_range, spkc_in_range, title_string):
     print('Found spikes %d'%(TPspk))
     print('Unfound spikes %d'%(totspk - TPspk))
     print('Extra spikes %d'%(totspk_ - TPspk))
+
+
+@deprecation.deprecated(details="Use detected_window_max_spike function instead")
+def detected_max_spike(m, x_, voltr_, thres=0.4):
+    pred_x_test = m.predict(x_)
+    window_length = x_.shape[1]
+    spkInWindow = pred_x_test>thres
+    spkInWindow = spkInWindow.flatten()
+    spkInWindow[:window_length] = False
+    spkcount_ = np.zeros(voltr_.shape[0]).astype(np.bool)
+    for idx, nspk in enumerate(spkInWindow):
+        if nspk:
+            x__ = voltr_[idx:idx+window_length]
+            spk_idx = np.where(x__ == x__.max()).squeeze()[0]
+            spkcount_[idx+spk_idx] = True
+    # diff = voltr_set - devoltr_set
+    # diff_ = diff[spkcount_.astype(np.bool)]
+    # thres = np.percentile(diff_, 20)
+    # spk_diff = diff > thres
+    # spkcount__ = np.logical_and(spkcount_, spk_diff)
+    return spkcount_
+
+@deprecation.deprecated(details="Use detected_window_max_spike function instead")
+def detected_peak_spikes(m, x_, voltr_, thres=0.4, devoltr_ = None, peakThres=.9, peak_minDist=10, smallPeakThres = 20):
+    import peakutils
+    pred_x_test = m.predict(x_)
+    window_length = x_.shape[1]
+    spkInWindow = pred_x_test>thres
+    spkInWindow = spkInWindow.flatten()
+    spkInWindow[:window_length] = False
+    spkcount_ = np.zeros(voltr_.shape[0]).astype(np.bool)
+    for idx, nspk in enumerate(spkInWindow):
+        if nspk:
+            x__ = voltr_[idx:idx+window_length]
+            spk_idx = peakutils.indexes(x__, thres=peakThres, min_dist=peak_minDist)
+            spkcount_[idx+spk_idx] = True
+    if (devoltr_ is not None) and spkcount_.sum()>0:
+        diff = voltr_ - devoltr_
+        diff_ = diff[spkcount_.astype(np.bool)]
+        thres = np.percentile(diff_, smallPeakThres)
+        spk_diff = diff > thres
+        spkcount_ = np.logical_and(spkcount_, spk_diff)
+    return spkcount_
