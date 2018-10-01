ba43017ff64f31825c7ea4fc6f23f8b55ee33baf
Ziqiang Wei <weiz@janelia.hhmi.org>
2018-10-01 19:28:56 -0400

    update with set up files

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='fish_proc',
+      version='0.1',
+      description='Utilities for analyzing imaging and behavioral data from zebrafish',
+      author='Ziqiang Wei',
+      author_email='weiz@janelia.hhmi.org',
+      packages=['fish_proc'],
+      url='https://github.com/zqwei/fish_processing',
+      zip_safe=False)
