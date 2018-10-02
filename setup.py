0e09c50739328de45834c9c291edfd79a794822d
Ziqiang Wei <weiz@janelia.hhmi.org>
2018-10-01 21:39:47 -0400

    structure the code for package

--- setup.py
+++ setup.py
@@ -5,6 +5,6 @@ setup(name='fish_proc',
       description='Utilities for analyzing imaging and behavioral data from zebrafish',
       author='Ziqiang Wei',
       author_email='weiz@janelia.hhmi.org',
-      packages=['fish_processing'],
+      packages=['fish_proc'],
       url='https://github.com/zqwei/fish_processing',
       zip_safe=False)
