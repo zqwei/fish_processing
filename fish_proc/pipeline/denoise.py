4b21b91ab2e9ce374c146b4bd071ce2b56ad0281
Ziqiang Wei <weiz@janelia.hhmi.org>
2018-10-03 00:24:28 -0400

    fix memory leak in detrend

--- fish_proc/pipeline/denoise.py
+++ fish_proc/pipeline/denoise.py
@@ -7,28 +7,37 @@ def detrend(Y_, fishName, n_split = 1):
     
     Y_trend = []
     for Y_split in np.array_split(Y_, n_split, axis=0):
-        Y_trend.append(parallel_to_chunks(trend, Y_split)[0])
+        Y_trend.append(parallel_to_chunks(trend, Y_split.astype('float32'))[0].astype('float32'))
     
     # Y_trend = parallel_to_chunks(trend, Y_split)
     # Y_trend = Y_trend[0]
     # Y_trend_ = tuple([_ for _ in Y_trend])
     # Y_trend_ = np.concatenate(Y_trend_, axis=0)
-    Y_trend = np.concatenate(Y_trend, axis=0)
-    Y_d = Y_ - Y_trend
-    np.save(f'{fishName}/Y_d', Y_d)
+    Y_trend = np.concatenate(Y_trend, axis=0).astype('float32')
+    # Y_d = Y_ - Y_trend
+    np.save(f'{fishName}/Y_d', Y_ - Y_trend)
     np.save(f'{fishName}/Y_trend', Y_trend)
-    Y_d = None
+    # Y_d = None
     Y_split = None
     Y_trend = None
-    clear_variables((Y_d, Y_split, Y_, Y_trend))
+    clear_variables((Y_split, Y_, Y_trend))
     get_process_memory();
     return None
 
 def denose_2dsvd(Y_d, fishName, nblocks=[10, 10], stim_knots=None, stim_delta=0):
     from ..denoiseLocalPCA.denoise import temporal as svd_patch
+    from ..utils.memory import get_process_memory, clear_variables
+    
     Y_d_ave = Y_d.mean(axis=-1, keepdims=True) # remove mean
     Y_d_std = Y_d.std(axis=-1, keepdims=True) # normalization
     Y_d = (Y_d - Y_d_ave)/Y_d_std
+    np.savez(f'{fishName}/Y_2dnorm', Y_d_ave=Y_d_ave, Y_d_std=Y_d_std)
+    Y_d_ave = None
+    Y_d_std = None
+    clear_variables((Y_d_ave, Y_d_std))
+    get_process_memory();
+    
+    
     dx=4
     maxlag=5
     confidence=0.99
@@ -37,6 +46,7 @@ def denose_2dsvd(Y_d, fishName, nblocks=[10, 10], stim_knots=None, stim_delta=0)
     mean_th_factor=1.15
     U_update=False
     min_rank=1
+    
     Y_svd, _ = svd_patch(Y_d, nblocks=nblocks, dx=dx, stim_knots=stim_knots, stim_delta=stim_delta)
-    np.savez(f'{fishName}/Y_2dsvd', Y_d=Y_d, Y_d_ave=Y_d_ave, Y_d_std=Y_d_std, Y_svd=Y_svd)
+    np.save(f'{fishName}/Y_2dsvd', Y_svd)
     return None
