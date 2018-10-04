dd40eb9b57a62eb482a22babd53ed4dd7108b84f
Ziqiang Wei <weiz@janelia.hhmi.org>
2018-10-04 14:31:05 -0400

    temporal solutions for local pca processing of long data

--- notebooks/DRN_processing_jobs.py
+++ notebooks/DRN_processing_jobs.py
@@ -178,6 +178,7 @@ def video_detrend():
 def local_pca():
     from fish_proc.pipeline.denoise import denose_2dsvd
     from pathlib import Path
+    from skimage.external.tifffile import imsave, imread
 
     dat_xls_file = pd.read_csv(dat_folder + 'Voltron Log_DRN_Exp.csv', index_col=0)
     for index, row in dat_xls_file.iterrows():
@@ -186,11 +187,29 @@ def local_pca():
         image_folder = f'/nrs/ahrens/Takashi/0{folder}/{fish}/'
         save_folder = dat_folder + f'{folder}/{fish}/Data'
         print(f'checking file {folder}/{fish}')
-        if not os.path.isfile(save_folder+'/Y_2dsvd.npy') and not os.path.isfile(save_folder+'/proc_local_denoise.tmp'):
-            if os.path.isfile(save_folder+'/Y_d.npy') or os.path.isfile(save_folder+'/Data/finished_detrend.tmp'):
+        if not os.path.isfile(save_folder+'/proc_local_denoise.tmp'):
+            if os.path.isfile(save_folder+'/finished_detrend.tmp'):
                 Path(save_folder+'/proc_local_denoise.tmp').touch()
-                Y_d = np.load(f'{save_folder}/Y_d.npy').astype('float32')
-                denose_2dsvd(Y_d, save_folder)
+
+                if os.path.isfile(f'{save_folder}/Y_d.npy'):
+                    Y_d = np.load(f'{save_folder}/Y_d.npy').astype('float32')
+                elif os.path.isfile(f'{save_folder}/Y_d.tif'):
+                    Y_d = imread(f'{save_folder}/Y_d.tif')
+
+                Y_d_ave = Y_d.mean(axis=-1, keepdims=True) # remove mean
+                Y_d_std = Y_d.std(axis=-1, keepdims=True) # normalization
+                Y_d = (Y_d - Y_d_ave)/Y_d_std
+                Y_d = Y_d.astype('float32')
+                np.savez_compressed(f'{save_folder}/Y_2dnorm', Y_d_ave=Y_d_ave, Y_d_std=Y_d_std)
+                Y_d_ave = None
+                Y_d_std = None
+                clear_variables((Y_d_ave, Y_d_std))
+                get_process_memory();
+
+                for n, Y_d_ in enumerate(np.array_split(Y_d, 10, axis=-1)):
+                    denose_2dsvd(Y_d_, save_folder, ext=f'{n}')
+
+                Y_d_ = None
                 Y_d = None
                 clear_variables(Y_d)
                 get_process_memory();
@@ -201,7 +220,7 @@ if __name__ == '__main__':
     if len(sys.argv)>1:
         eval(sys.argv[1]+"()")
     else:
-        video_detrend()
+        local_pca()
 
     # update_table(update_ods = False)
     # swim()
