b06b913cd0b8757b9c7e0f40ec22fe85758f38bf
Ziqiang Wei <weiz@janelia.hhmi.org>
2018-10-27 12:40:37 -0400

    update local codes for corraltion estimation and noise

--- fish_proc/utils/snr.py
+++ fish_proc/utils/snr.py
@@ -1,18 +1,7 @@
 import numpy as np
 import cv2
-import scipy.ndimage.filters as filters
 from scipy.ndimage.filters import convolve
-
-from mpl_toolkits.axes_grid1 import make_axes_locatable
-import matplotlib.pyplot as plt
-
-#import caiman as cm
-import trefide.extras.tools as tools_
-import trefide.extras.tool_grid as tgrid
-
-import trefide.extras.denoise as denoise
-# update noise estimators
-# clean up comments after revision
+import .noise_estimator as noise_estimator
 
 
 def correlation_pnr(Y,
@@ -45,66 +34,16 @@ def correlation_pnr(Y,
             peak-to-noise ratios of all pixels/voxels
 
     """
-    #if swap_dim:
-    #    Y = np.transpose(
-    #        Y, tuple(np.hstack((Y.ndim - 1,
-    #            list(range(Y.ndim))[:-1]))))
-
-    # parameters
-    #_, d1, d2 = Y.shape
-
-
-    #data_raw = Y.reshape(-1, d1, d2).astype('float32')
-
-    # filter data
-    #data_filtered = data_raw.copy()
-    #if gSig:
-    #    if not isinstance(gSig, list):
-    #        gSig = [gSig, gSig]
-    #    ksize = tuple([(3 * i) // 2 * 2 + 1 for i in gSig])
-        # create a spatial filter for removing background
-        # psf = gen_filter_kernel(width=ksize, sigma=gSig, center=center_psf)
-
-    #    if center_psf:
-    #        for idx, img in enumerate(data_filtered):
-    #            data_filtered[idx, ] = cv2.GaussianBlur(img,
-    #                                                    ksize=ksize,
-    #                                                    sigmaX=gSig[0],
-    #                                                    sigmaY=gSig[1],
-    #                                                    borderType=1) \
-    #                - cv2.boxFilter(img, ddepth=-1, ksize=ksize, borderType=1)
-    #        # data_filtered[idx, ] = cv2.filter2D(img, -1, psf, borderType=1)
-    #    else:
-    #        for idx, img in enumerate(data_filtered):
-    #            data_filtered[idx, ] = cv2.GaussianBlur(
-    #                img, ksize=ksize, sigmaX=gSig[0], sigmaY=gSig[1], borderType=1)
-
-    # compute peak-to-noise ratio
-    #data_filtered -= np.mean(data_filtered, axis=0)
     Y = Y - Y.mean(2,keepdims=True)
-    #data_max = np.max(data_filtered, axis=0)
     data_max = Y.max(2)#,keepdims=True)
-    #data_std = noise_estimator.get_noise_fft(data_filtered.transpose())[0].transpose()
-    #data_std = noise_estimator.get_noise_fft(Y)[0]
     data_std = denoise.noise_level(Y)#[0]
-    # Update to match noise from denoise.py here
-
-    ## data_std = get_noise(data_filtered, method='diff2_med')
     pnr = np.divide(data_max, data_std)
     if remove_small_val:
         pnr[pnr < 0] = 0
-
     tmp_data = Y / data_std[:,:,np.newaxis]
-    # remove small values
-    #tmp_data = data_filtered.copy() / data_std
     if remove_small_val:
         tmp_data[tmp_data < remove_small_val_th] = 0
-
-    # compute correlation image
-    # cn = local_correlation(tmp_data, d1=d1, d2=d2)
-    #cn = local_correlations_fft(tmp_data, swap_dim=False)
     cn = local_correlations_fft(tmp_data, swap_dim=True)
-
     return cn, pnr
 
 
@@ -175,3 +114,19 @@ def local_correlations_fft(Y,
             np.ones(Y.shape[1:], dtype='float32'), sz, mode='constant')
     Cn = np.mean(Yconv * Y, axis=0) / MASK
     return Cn
+
+def noise_level(mov_wf,
+                range_ff =[0.25,0.5]):
+    """
+    Calculate noise level in movie pixels
+    """
+    ndim_ = np.ndim(mov_wf)
+    if ndim_==3:
+        dims_ = mov_wf.shape
+        mov_wf = mov_wf.reshape((np.prod(dims_[:2]), dims_[2]),order='F')
+    noise_level = noise_estimator.noise_estimator(mov_wf,
+                                                  method='logmexp')
+    if ndim_ ==3:
+        noise_level = noise_level.reshape(dims_[:2], order='F')
+
+    return noise_level
