55208398a8828495babd22d2c7a6b966804cf54b
Ziqiang Wei <weiz@janelia.hhmi.org>
2018-11-20 11:05:43 -0500

    update processing files

--- notebooks/DRN_processing_jobs.py
+++ notebooks/DRN_processing_jobs.py
@@ -3,32 +3,33 @@ import pandas as pd
 import os, sys
 from fish_proc.utils.memory import get_process_memory, clear_variables
 
-dat_folder = '/nrs/ahrens/Ziqiang/Takashi_DRN_project/'
+dat_folder = '/nrs/ahrens/Ziqiang/Takashi_DRN_project/ProcessedData/'
 cameraNoiseMat = '/groups/ahrens/ahrenslab/Ziqiang/gainMat/gainMat20180208'
 
 
-def update_table(update_ods = False):
-    '''
-    Update Voltron Log_DRN_Exp.csv
-    run when new data is added
-    '''
-    if update_ods:
-        dat_xls_file = pd.read_excel(dat_folder+'Voltron Log_DRN_Exp.xlsx')
-        dat_xls_file = dat_xls_file.dropna(how='all').reset_index()
-        dat_xls_file['folder'] = dat_xls_file['folder'].astype('int').astype('str')
-        dat_xls_file['finished'] = False
-        dat_xls_file.to_csv(dat_folder + 'Voltron Log_DRN_Exp.csv')
-    return None
-
+# def update_table(update_ods = False):
+#     '''
+#     Update Voltron Log_DRN_Exp.csv
+#     run when new data is added
+#     '''
+#     if update_ods:
+#         dat_xls_file = pd.read_excel(dat_folder+'Voltron Log_DRN_Exp.xlsx')
+#         dat_xls_file = dat_xls_file.dropna(how='all').reset_index()
+#         dat_xls_file['folder'] = dat_xls_file['folder'].astype('int').astype('str')
+#         dat_xls_file['finished'] = False
+#         dat_xls_file.to_csv('../Voltron Log_DRN_Exp.csv')
+#     return None
 
 def monitor_process():
     '''
     Update Voltron Log_DRN_Exp.csv
     monitor process of processing
     '''
-    dat_xls_file = pd.read_csv(dat_folder + 'Voltron Log_DRN_Exp.csv', index_col=0)
+    dat_xls_file = pd.read_csv('../Voltron Log_DRN_Exp.csv', index_col=0)
     if 'index' in dat_xls_file.columns:
         dat_xls_file = dat_xls_file.drop('index', axis=1)
+    dat_xls_file['folder'] = dat_xls_file['folder'].astype(int).apply(str)
+
     if not 'spikes' in dat_xls_file.columns:
         dat_xls_file['spikes']=False
     if not 'subvolt' in dat_xls_file.columns:
@@ -59,25 +60,26 @@ def monitor_process():
         if os.path.exists(save_folder+'/Data/finished_subvolt.tmp'):
             dat_xls_file.at[index, 'subvolt'] = True
     print(dat_xls_file.sum(numeric_only=True))
-    dat_xls_file.to_csv(dat_folder + 'Voltron Log_DRN_Exp.csv')
-    # save a local copy
-    dat_xls_file.to_csv('Voltron Log_DRN_Exp.csv')
+    dat_xls_file.to_csv('../Voltron Log_DRN_Exp.csv')
     return None
+
 
 def swim():
     '''
     Processing swim using TK's code
     '''
     from fish_proc.utils.ep import process_swim
-    dat_xls_file = pd.read_csv(dat_folder + 'Voltron Log_DRN_Exp.csv', index_col=0)
+    dat_xls_file = pd.read_csv('../Voltron Log_DRN_Exp.csv', index_col=0)
+    dat_xls_file['folder'] = dat_xls_file['folder'].apply(lambda x: f'{x:0>8}')
     for _, row in dat_xls_file.iterrows():
         folder = row['folder']
         fish = row['fish']
-        swim_chFit = f'/nrs/ahrens/Takashi/0{folder}/{fish}.10chFlt'
+        swim_chFit = f'/nrs/ahrens/Takashi/{folder}/{fish}.10chFlt'
         save_folder = dat_folder + f'{folder}/{fish}/'
         if not os.path.exists(save_folder):
             os.makedirs(save_folder)
         if not os.path.exists(save_folder+'/swim'):
+            print(f'checking file {folder}/{fish}')
             try:
                 process_swim(swim_chFit, save_folder)
             except IOError:
@@ -91,14 +93,16 @@ def pixel_denoise():
     Generate files -- imgDNoMotion.tif, motion_fix_.npy
     '''
     from fish_proc.pipeline.preprocess import pixel_denoise, pixel_denoise_img_seq
-    dat_xls_file = pd.read_csv(dat_folder + 'Voltron Log_DRN_Exp.csv', index_col=0)
+    dat_xls_file = pd.read_csv('../Voltron Log_DRN_Exp.csv', index_col=0)
+    dat_xls_file['folder'] = dat_xls_file['folder'].apply(lambda x: f'{x:0>8}')
     for index, row in dat_xls_file.iterrows():
         folder = row['folder']
         fish = row['fish']
-        image_folder = f'/nrs/ahrens/Takashi/0{folder}/{fish}/'
+        image_folder = row['rootDir'] + f'{folder}/{fish}/'
         fish_folder = dat_folder + f'{folder}/{fish}/'
         save_folder = dat_folder + f'{folder}/{fish}/Data'
-        if os.path.exists(fish_folder):
+
+        if os.path.exists(image_folder):
             print(f'checking file {folder}/{fish}')
             if not os.path.exists(save_folder+'/'):
                 os.makedirs(save_folder)
@@ -131,10 +135,8 @@ def registration(is_largefile=True):
     from pathlib import Path
     from fish_proc.pipeline.preprocess import motion_correction
     from skimage.io import imread, imsave
-    dat_xls_file = pd.read_csv(dat_folder + 'Voltron Log_DRN_Exp.csv', index_col=0)
-    if 'index' in dat_xls_file.columns:
-        dat_xls_file = dat_xls_file.drop('index', axis=1)
-    dat_xls_file['folder'] = dat_xls_file['folder'].astype(int).apply(str)
+    dat_xls_file = pd.read_csv('../Voltron Log_DRN_Exp.csv', index_col=0)
+    dat_xls_file['folder'] = dat_xls_file['folder'].apply(lambda x: f'{x:0>8}')
 
     for index, row in dat_xls_file.iterrows():
         folder = row['folder']
@@ -183,8 +185,9 @@ def video_detrend():
     from pathlib import Path
     from multiprocessing import cpu_count
     from skimage.io import imsave, imread
+    dat_xls_file = pd.read_csv('../Voltron Log_DRN_Exp.csv', index_col=0)
+    dat_xls_file['folder'] = dat_xls_file['folder'].apply(lambda x: f'{x:0>8}')
 
-    dat_xls_file = pd.read_csv(dat_folder + 'Voltron Log_DRN_Exp.csv', index_col=0)
     for index, row in dat_xls_file.iterrows():
         folder = row['folder']
         fish = row['fish']
@@ -227,8 +230,9 @@ def local_pca():
     from fish_proc.pipeline.denoise import denose_2dsvd
     from pathlib import Path
     from skimage.external.tifffile import imsave, imread
+    dat_xls_file = pd.read_csv('../Voltron Log_DRN_Exp.csv', index_col=0)
+    dat_xls_file['folder'] = dat_xls_file['folder'].apply(lambda x: f'{x:0>8}')
 
-    dat_xls_file = pd.read_csv(dat_folder + 'Voltron Log_DRN_Exp.csv', index_col=0)
     for index, row in dat_xls_file.iterrows():
         folder = row['folder']
         fish = row['fish']
@@ -268,13 +272,142 @@ def local_pca():
     return None
 
 
-def demix_components():
-    dat_xls_file = pd.read_csv(dat_folder + 'Voltron Log_DRN_Exp.csv', index_col=0)
+def demix_middle_data():
+    import matplotlib.pyplot as plt
+    import seaborn as sns
+    from pathlib import Path
+    from skimage.external.tifffile import imsave, imread
+    from fish_proc.demix import superpixel_analysis as sup
+    from fish_proc.utils.snr import correlation_pnr
+    import pickle
+
+    sns.set(font_scale=2)
+    sns.set_style("white")
+    dat_xls_file = pd.read_csv('../Voltron Log_DRN_Exp.csv', index_col=0)
+    dat_xls_file['folder'] = dat_xls_file['folder'].apply(lambda x: f'{x:0>8}')
+
     for index, row in dat_xls_file.iterrows():
-        demix_middle_data_with_mask(row)
+        folder = row['folder']
+        fish = row['fish']
+        image_folder = f'/nrs/ahrens/Takashi/0{folder}/{fish}/'
+        save_folder = dat_folder + f'{folder}/{fish}/Data'
+        save_image_folder = dat_folder + f'{folder}/{fish}/Results'
+
+        if not os.path.exists(save_image_folder):
+            os.makedirs(save_image_folder)
+        print('=====================================')
+        print(save_folder)
+
+        if os.path.isfile(save_folder+'/finished_demix.tmp'):
+            continue
+
+        if not os.path.isfile(save_folder+'/proc_demix.tmp'):
+            Path(save_folder+'/proc_demix.tmp').touch()
+            _ = np.load(f'{save_folder}/Y_2dnorm.npz')
+            Y_d_ave= _['Y_d_ave']
+            Y_d_std= _['Y_d_std']
+            if not os.path.isfile(f'{save_folder}/Y_svd.tif'):
+                Y_svd = []
+                for n_ in range(10):
+                    Y_svd.append(np.load(f'{save_folder}/Y_2dsvd{n_}.npy').astype('float32'))
+                Y_svd = np.concatenate(Y_svd, axis=-1)
+                print(Y_svd.shape)
+                imsave(f'{save_folder}/Y_svd.tif', Y_svd.astype('float32'), compress=1)
+                print('Concatenate files into a tif file')
+            else:
+                Y_svd = imread(f'{save_folder}/Y_svd.tif').astype('float32')
+
+            for n_ in range(10):
+                if os.path.isfile(f'{save_folder}/Y_2dsvd{n_}.npy'):
+                    os.remove(f'{save_folder}/Y_2dsvd{n_}.npy')
+            get_process_memory();
+
+            plt.figure(figsize=(8, 4))
+            plt.imshow(Y_d_std[:, :, 0])
+            plt.colorbar()
+            plt.tight_layout()
+            plt.savefig(f'{save_image_folder}/Detrend_std.png')
+            plt.close()
+
+            len_Y = Y_svd.shape[-1]
+            Y_svd_ = Y_svd[:, :, len_Y//4:-len_Y//4]
+            d1, d2, _ = Y_svd_.shape
+            Y_svd = None
+            clear_variables(Y_svd)
+            get_process_memory();
+
+            mov_ = Y_svd_*Y_d_std + np.random.normal(size=Y_svd_.shape)*0.7
+            Y_svd_ = None
+            clear_variables(Y_svd_)
+            Cn, _ = correlation_pnr(-mov_, gSig=None, remove_small_val=False, remove_small_val_th=3, center_psf=False)
+            plt.figure(figsize=(8, 4))
+            plt.imshow(Cn, vmin=0.0)
+            plt.colorbar()
+            plt.tight_layout()
+            plt.savefig(f'{save_image_folder}/Denoised_local_corr_noised.png')
+            plt.close()
+
+            pass_num = 4
+            cut_off_point=np.percentile(Cn.ravel(), [99, 95, 90, 85])
+            rlt_= sup.demix_whole_data(-mov_, cut_off_point, length_cut=[25,25,25,25],
+                                       th=[2,2,1,1], pass_num=pass_num, residual_cut = [0.6,0.6,0.6,0.6],
+                                       corr_th_fix=0.3, max_allow_neuron_size=0.2, merge_corr_thr=0.35,
+                                       merge_overlap_thr=0.6, num_plane=1, patch_size=[100, 100], plot_en=False,
+                                       TF=False, fudge_factor=1, text=False, bg=False, max_iter=60,
+                                       max_iter_fin=100, update_after=4)
+
+            with open(f'{save_folder}/period_Y_demix_rlt.pkl', 'wb') as f:
+                pickle.dump(rlt_, f)
+
+            print('Result file saved?')
+            print(os.path.isfile(f'{save_folder}/period_Y_demix_rlt.pkl'))
+
+            with open(f'{save_folder}/period_Y_demix_rlt.pkl', 'rb') as f:
+                rlt_ = pickle.load(f)
+
+            for n_pass in range(pass_num):
+                sup.pure_superpixel_single_plot(rlt_["superpixel_rlt"][n_pass]["connect_mat_1"],
+                                                rlt_["superpixel_rlt"][n_pass]["unique_pix"],
+                                                rlt_["superpixel_rlt"][n_pass]["brightness_rank_sup"],
+                                                text=True,
+                                                pure=False);
+                plt.savefig(f'{save_image_folder}/Demixed_pass_#{n_pass}.png')
+                plt.close()
+
+            Y_mean = imread(f'{save_folder}/imgDMotion.tif').mean(axis=0)
+            Y_d_mean = imread(f'{save_folder}/Y_d.tif').mean(axis=-1)
+            Y_trend_ave = Y_mean - Y_d_mean
+            Y_mean = None
+            Y_d_mean = None
+            Y_svd_ = None
+            clear_variables((Y_d_mean, Y_mean, mov_))
+
+            np.save(f'{save_folder}/Y_trend_ave', Y_trend_ave)
+
+            A = rlt_['fin_rlt']['a']
+            A_ = A[:, (A>0).sum(axis=0)>0]
+            A_comp = np.zeros(A_.shape[0])
+            A_comp[A_.sum(axis=-1)>0] = np.argmax(A_[A_.sum(axis=-1)>0, :], axis=-1) + 1
+            plt.figure(figsize=(8,4))
+            plt.imshow(Y_trend_ave, cmap=plt.cm.gray)
+            plt.imshow(A_comp.reshape(d2, d1).T, cmap=plt.cm.nipy_spectral_r, alpha=0.7)
+            plt.axis('off')
+            plt.tight_layout()
+            plt.savefig(f'{save_image_folder}/Demixed_components.png')
+            plt.close()
+            Path(save_folder+'/finished_demix.tmp').touch()
     return None
 
-def demix_middle_data_with_mask(row):
+
+def demix_components(ext=''):
+    dat_xls_file = pd.read_csv('../Voltron Log_DRN_Exp.csv', index_col=0)
+    dat_xls_file['folder'] = dat_xls_file['folder'].apply(lambda x: f'{x:0>8}')
+    for index, row in dat_xls_file.iterrows():
+        demix_middle_data_with_mask(row, ext=ext)
+    return None
+
+
+def demix_middle_data_with_mask(row, ext=''):
     import matplotlib.pyplot as plt
     import seaborn as sns
     from pathlib import Path
@@ -289,7 +422,7 @@ def demix_middle_data_with_mask(row):
 
     folder = row['folder']
     fish = row['fish']
-    image_folder = f'/nrs/ahrens/Takashi/0{folder}/{fish}/'
+    image_folder = f'/nrs/ahrens/Takashi/{folder}/{fish}/'
     save_folder = dat_folder + f'{folder}/{fish}/Data'
     save_image_folder = dat_folder + f'{folder}/{fish}/Results'
     if not os.path.exists(save_image_folder):
@@ -297,12 +430,11 @@ def demix_middle_data_with_mask(row):
     print('=====================================')
     print(save_folder)
 
-    if os.path.isfile(save_folder+'/finished_demix_with_mask.tmp'):
+    if os.path.isfile(save_folder+f'/finished_demix{ext}.tmp'):
         return None
 
-    # if not os.path.isfile(save_folder+'/proc_demix_with_mask.tmp'):
-    if True:
-        Path(save_folder+'/proc_demix.tmp').touch()
+    if not os.path.isfile(save_folder+f'/proc_demix{ext}.tmp'):
+        Path(save_folder+f'/proc_demix{ext}.tmp').touch()
         _ = np.load(f'{save_folder}/Y_2dnorm.npz')
         Y_d_ave= _['Y_d_ave']
         Y_d_std= _['Y_d_std']
@@ -364,16 +496,16 @@ def demix_middle_data_with_mask(row):
                                    TF=False, fudge_factor=1, text=False, bg=False, max_iter=60,
                                    max_iter_fin=100, update_after=20)
 
-        with open(f'{save_folder}/period_Y_demix_with_mask_rlt.pkl', 'wb') as f:
+        with open(f'{save_folder}/period_Y_demix{ext}_rlt.pkl', 'wb') as f:
             pickle.dump(rlt_, f)
 
         print('Result file saved?')
-        print(os.path.isfile(f'{save_folder}/period_Y_demix_with_mask_rlt.pkl'))
+        print(os.path.isfile(f'{save_folder}/period_Y_demix{ext}_rlt.pkl'))
 
-        with open(f'{save_folder}/period_Y_demix_with_mask_rlt.pkl', 'rb') as f:
+        with open(f'{save_folder}/period_Y_demix{ext}_rlt.pkl', 'rb') as f:
             rlt_ = pickle.load(f)
 
-        if os.path.isfile(f'{save_folder}/Y_trend_ave.npy'):
+        if not os.path.isfile(f'{save_folder}/Y_trend_ave.npy'):
             Y_mean = imread(f'{save_folder}/imgDMotion.tif').mean(axis=0)
             Y_d_mean = imread(f'{save_folder}/Y_d.tif').mean(axis=-1)
             Y_trend_ave = Y_mean - Y_d_mean
@@ -396,20 +528,17 @@ def demix_middle_data_with_mask(row):
         plt.imshow(A_comp.reshape(d2, d1).T, cmap=plt.cm.nipy_spectral_r, alpha=0.7)
         plt.axis('off')
         plt.tight_layout()
-        plt.savefig(f'{save_image_folder}/Demixed_components_with_mask.png')
+        plt.savefig(f'{save_image_folder}/Demixed_components{ext}.png')
         plt.close()
-        Path(save_folder+'/finished_demix_with_mask.tmp').touch()
+        Path(save_folder+f'/finished_demix{ext}.tmp').touch()
     return None
 
 
 if __name__ == '__main__':
     if len(sys.argv)>1:
-        eval(sys.argv[1]+"()")
+        ext = ''
+        if len(sys.argv)>2:
+            ext = sys.argv[2]
+        eval(sys.argv[1]+f"({ext})")
     else:
-        update_table(update_ods = False)
-        # swim()
-        # pixel_denoise()
-        # registration()
-        # video_detrend()
-        # local_pca()
-        # demix_middle_data()
+        monitor_process()
