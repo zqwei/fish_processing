467c425be44ab94fbc1ef3449c8c4b3a07ac2a5f
Ziqiang Wei <weiz@janelia.hhmi.org>
2019-11-11 16:23:30 -0500

    add tar operation of folders

---
+++ fish_proc/utils/fileio.py
@@ -0,0 +1,4 @@
+def make_tarfile(output_filename, source_dir):
+    import tarfile
+    with tarfile.open(output_filename, "w:gz") as tar:
+        tar.add(source_dir, arcname=os.path.basename(source_dir))
