8150a1ddd92c22dfca75f44c0059194f3c67de50
Ziqiang Wei <weiz@janelia.hhmi.org>
2019-11-13 16:33:12 -0500

    fix import error in file-io

--- fish_proc/utils/fileio.py
+++ fish_proc/utils/fileio.py
@@ -1,4 +1,5 @@
 def make_tarfile(output_filename, source_dir):
     import tarfile
+    import os
     with tarfile.open(output_filename, "w:gz") as tar:
         tar.add(source_dir, arcname=os.path.basename(source_dir))
