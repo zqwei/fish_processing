ca9229cfecc5f4a78df04ea553b5471443a0701a
Ziqiang Wei <weiz@janelia.hhmi.org>
2019-12-19 16:42:25 -0500

    add du function in file operations

--- fish_proc/utils/fileio.py
+++ fish_proc/utils/fileio.py
@@ -3,3 +3,9 @@ def make_tarfile(output_filename, source_dir):
     import os
     with tarfile.open(output_filename, "w:gz") as tar:
         tar.add(source_dir, arcname=os.path.basename(source_dir))
+
+        
+def du(path):
+    import subprocess
+    # return subprocess.check_output(['du','-sh', path]).split()[0]
+    return subprocess.check_output(['du', path]).split()[0]
